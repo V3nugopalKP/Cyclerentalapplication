e5e9c7ffe3c90534b0342c28a17b7801cdedaa92
SREERAM2612 <92304665+SREERAM2612@users.noreply.github.com>
2024-11-06 10:46:23 +0530

    Eleventh Commit

--- cycle_rental_app_angular/src/app/modules/admin/components/update-cycle/update-cycle.component.ts
+++ cycle_rental_app_angular/src/app/modules/admin/components/update-cycle/update-cycle.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AdminService } from '../../services/admin.service';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NzSpinModule } from 'ng-zorro-antd/spin';
@@ -9,23 +9,32 @@ import { NzSelectModule } from 'ng-zorro-antd/select';
 import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
 import { CommonModule } from '@angular/common';
 import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-update-cycle',
   standalone: true,
-  imports: [NzSpinModule, NzButtonModule,NzInputModule, NzFormModule, ReactiveFormsModule, NzSelectModule, NzDatePickerModule, CommonModule],
+  imports: [
+    NzSpinModule,
+    NzButtonModule,
+    NzInputModule,
+    NzFormModule,
+    ReactiveFormsModule,
+    NzSelectModule,
+    NzDatePickerModule,
+    CommonModule
+  ],
   templateUrl: './update-cycle.component.html',
-  styleUrl: './update-cycle.component.scss'
+  styleUrls: ['./update-cycle.component.scss']
 })
 export class UpdateCycleComponent {
   isSpinning = false;
   cycleId!: number;
-  imgChanged: boolean =  false;
+  imgChanged: boolean = false;
   selectedFile: any;
-  imagePreview: string | ArrayBuffer | null;
+  imagePreview: string | ArrayBuffer | null = null;
   existingImage: string | null = null;
   updateForm!: FormGroup;
-  listOfOption: Array<{ label: string; value: string }> = [];
   listOfBrands = ["Trek", "Giant", "Specialized", "Cannondale", "Bianchi", "Cervelo", "Santa Cruz", "Yeti"];
   listOfType = ["Road Bike", "Mountain Bike", "Hybrid Bike", "Electric Bike", "BMX", "Gravel Bike"];
   listOfColor = ["Red", "White", "Blue", "Black", "Orange", "Grey", "Silver"];
@@ -41,7 +50,7 @@ export class UpdateCycleComponent {
 
   ngOnInit() {
     this.cycleId = this.activatedRoute.snapshot.params['id'];
-    this.updateForm = this.fb.group({  // Added parentheses around the form group object
+    this.updateForm = this.fb.group({
       name: [null, Validators.required],
       brand: [null, Validators.required],
       type: [null, Validators.required],
@@ -50,7 +59,7 @@ export class UpdateCycleComponent {
       price: [null, Validators.required],
       description: [null, Validators.required],
       year: [null, Validators.required],
-      image: [null, Validators.required]  // Added image control
+      image: [null, Validators.required]
     });
     this.getCycleById();
   }
@@ -58,58 +67,57 @@ export class UpdateCycleComponent {
   getCycleById() {
     this.isSpinning = true;
     this.adminService.getCycleById(this.cycleId).subscribe((res) => {
-      // console.log(res);
       this.isSpinning = false;
       const cycleDto = res;
       this.existingImage = 'data:image/jpeg;base64,' + res.returnedImage;
       this.updateForm.patchValue(cycleDto);
     });
   }
-}
-updateCycle() {
-  this.isSpinning = true;
 
-  const formData: FormData = new FormData();
-  if (this.imgChanged && this.selectedFile) {
-    formData.append('image', this.selectedFile);
+  updateCycle() {
+    this.isSpinning = true;
+
+    const formData: FormData = new FormData();
+    if (this.imgChanged && this.selectedFile) {
+      formData.append('image', this.selectedFile);
+    }
+
+    formData.append('brand', this.updateForm.get('brand')!.value);
+    formData.append('name', this.updateForm.get('name')!.value);
+    formData.append('type', this.updateForm.get('type')!.value);
+    formData.append('color', this.updateForm.get('color')!.value);
+    formData.append('year', this.updateForm.get('year')!.value);
+    formData.append('transmission', this.updateForm.get('transmission')!.value);
+    formData.append('description', this.updateForm.get('description')!.value);
+    formData.append('price', this.updateForm.get('price')!.value);
+
+    this.adminService.updateCycle(this.cycleId, formData).subscribe(
+      (res) => {
+        this.isSpinning = false;
+        this.message.success('Cycle updated successfully', { nzDuration: 5000 });
+        this.router.navigateByUrl('/admin/dashboard');
+      },
+      (error) => {
+        this.isSpinning = false;
+        this.message.error('Error while updating cycle', { nzDuration: 5000 });
+      }
+    );
   }
 
-  formData.append('brand', this.updateForm.get('brand').value);
-  formData.append('name', this.updateForm.get('name').value);
-  formData.append('type', this.updateForm.get('type').value);
-  formData.append('color', this.updateForm.get('color').value);
-  formData.append('year', this.updateForm.get('year').value);
-  formData.append('transmission', this.updateForm.get('transmission').value);
-  formData.append('description', this.updateForm.get('description').value);
-  formData.append('price', this.updateForm.get('price').value);
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+    this.imgChanged = true;
+    this.existingImage = null;
+    this.previewImage();
+  }
 
-  console.log(formData);
-  this.adminService.updateCycle(this.carId, formData).subscribe(
-    (res) => {
-      this.isSpinning = false;
-      this.message.success('Cycle updated successfully', { nzDuration: 5000 });
-      this.router.navigateByUrl('/admin/dashboard');
-      console.log(res);
-    },
-    (error) => {
-      this.message.error('Error while updating cycle', { nzDuration: 5000 });
-    }
-  );
-}
+  previewImage() {
+    const reader = new FileReader();
 
-onFileSelected(event: any) {
-  this.selectedFile = event.target.files[0];
-  this.imgChanged = true;
-  this.existingImage = null;
-  this.previewImage();
-}
+    reader.onload = () => {
+      this.imagePreview = reader.result;
+    };
 
-previewImage() {
-  const reader = new FileReader();
-
-  reader.onload = () => {
-    this.imagePreview = reader.result;
-  };
-
-  reader.readAsDataURL(this.selectedFile);
+    reader.readAsDataURL(this.selectedFile);
+  }
 }
